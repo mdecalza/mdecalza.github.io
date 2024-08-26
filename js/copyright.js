7dd24e65b17cf91485a095ff2d796b3be5a81869
Matthew DeCalzadilla <matt.decalzadilla@gmail.com>
2024-08-26 17:59:01 -0600

    Added a script to auto-update the copyright year at the footer of each page.

---
+++ js/copyright.js
@@ -0,0 +1,16 @@
+/* Copyright 2024 Matthew DeCalzadilla */
+
+// Update the copyright year in the footer
+function updateCopyright() {
+    const today = new Date(Date.now());
+    const thisYear = today.getFullYear().toString();
+
+    if (thisYear === "2024") {
+        document.getElementById("footer-copyright").innerText = "© Matthew DeCalzadilla 2024";
+    }
+    else {
+        document.getElementById("footer-copyright").innerText = "© Matthew DeCalzadilla 2024 - " + thisYear;
+    }
+}
+
+updateCopyright();  // on start
