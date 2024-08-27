5ebc7698782b185c07a623572e1fde45e6f611d1
Matthew DeCalzadilla <matt.decalzadilla@gmail.com>
2024-08-26 18:53:36 -0600

    Refactor copyright JS script

--- js/copyright.js
+++ js/copyright.js
@@ -1,16 +1,12 @@
 /* Copyright 2024 Matthew DeCalzadilla */
 
 // Update the copyright year in the footer
-function updateCopyright() {
-    const today = new Date(Date.now());
-    const thisYear = today.getFullYear().toString();
+const today = new Date(Date.now());
+const thisYear = today.getFullYear().toString();
 
-    if (thisYear === "2024") {
-        document.getElementById("footer-copyright").innerText = "© Matthew DeCalzadilla 2024";
-    }
-    else {
-        document.getElementById("footer-copyright").innerText = "© Matthew DeCalzadilla 2024 - " + thisYear;
-    }
+if (thisYear === "2024") {
+    document.getElementById("footer-copyright").innerText = "© Matthew DeCalzadilla 2024";
 }
-
-updateCopyright();  // on start
+else {
+    document.getElementById("footer-copyright").innerText = "© Matthew DeCalzadilla 2024 - " + thisYear;
+}
