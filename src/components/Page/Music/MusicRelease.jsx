6b7d97809ed483b1534e8c845db5594d371cd2d0
Matthew DeCalzadilla <matt.decalzadilla@gmail.com>
2024-12-05 15:52:02 -0700

    Made scaling of Scroller UI elements more responsive

--- src/components/Page/Music/MusicRelease.jsx
+++ src/components/Page/Music/MusicRelease.jsx
@@ -12,22 +12,25 @@ MusicRelease.propTypes = {
 export default function MusicRelease({title = '', subtitle = '', releaseDate = '', type = '', genre = '', coverImageSrc = ''}) {
     return (
         <div
-        className='MusicRelease mx-5'>
+        className='MusicRelease d-flex flex-column mx-5 justify-content-center'>
             <img
             src={coverImageSrc}
-            className='w-75 m-5 mx-auto rounded-2 border border-3 border-light border-opacity-75' />
+            className='AlbumArt mx-auto my-5 rounded-2 border border-3 border-light border-opacity-75' />
             <div
-            className='card bg-light px-5 py-3 w-100'>
-                <p
-                className='fs-4 mb-0'>
-                    <b><i>{title}</i></b><br />
-                </p>
-                <p>
-                    <b className='fs-6'><i>{subtitle}</i></b><br />
-                    {type}<br />
-                    <i>{genre}</i><br />
-                    Released: {releaseDate}
-                </p>
+            className='AlbumCard card bg-light w-fit-content mx-auto px-4 py-3'>
+                <div
+                className='my-auto'>
+                    <p
+                    className='fs-4 mb-0'>
+                        <b><i>{title}</i></b><br />
+                    </p>
+                    <p>
+                        <b className='fs-6'><i>{subtitle}</i></b><br />
+                        {type}<br />
+                        <i>{genre}</i><br />
+                        Released: {releaseDate}
+                    </p>
+                </div>
             </div>
         </div>
     );
