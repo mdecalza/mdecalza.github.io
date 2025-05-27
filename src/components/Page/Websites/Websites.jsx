910c7fef69e487676fcfe1e4658bab9ff23622be
Matthew DeCalzadilla <matt.decalzadilla@gmail.com>
2025-05-26 23:06:04 -0600

    Change website thumbnail images to live views

--- src/components/Page/Websites/Websites.jsx
+++ src/components/Page/Websites/Websites.jsx
@@ -1,13 +1,9 @@
 /* Copyright © 2024—2025 Matthew DeCalzadilla */
 
-/* assets */
-import casaFrescaThumbnail from '../../../assets/websites/casafrescapage.png';
-import itsMateoThumbnail from '../../../assets/websites/itsmateopage.png';
-
 /* components */
 import Header from '../../Header/Header';
 import Socials from '../../Socials/Socials';
-import Thumbnail from '../../Thumbnail/Thumbnail';
+import WebsiteThumbnail from '../../Thumbnail/WebsiteThumbnail';
 
 export default function Websites() {
 
@@ -18,12 +14,10 @@ export default function Websites() {
             title='Websites' />
             <Socials
             socialsList={[ 'linkedin', 'main-email', 'github' ]} />
-            <Thumbnail
-            to={{ pathname: 'https://casafrescacleaning.com/' }}
-            imgSrc={ casaFrescaThumbnail } />
-            <Thumbnail
-            to={{ pathname: '/' }}
-            imgSrc={ itsMateoThumbnail } />
+            <WebsiteThumbnail
+            to={{ pathname: 'https://casafrescacleaning.com' }} />
+            <WebsiteThumbnail
+            to={{ pathname: '/' }} />
         </div>
     );
 }
---
+++ src/components/Thumbnail/WebsiteThumbnail.jsx
@@ -0,0 +1,39 @@
+/* Copyright © 2024—2025 Matthew DeCalzadilla */
+
+import { Link } from 'react-router';
+
+WebsiteThumbnail.propTypes = {
+    to: { pathname: '/', },
+    isInnerLink: false
+};
+
+export default function WebsiteThumbnail({ to = { pathname: '/' }, isInnerLink = !to.pathname.includes('://') }) {
+    const thumbnailButtonClasses = 'mx-auto my-3 btn btn-transparent growOnHover fadeInDelayed';
+    const thumbnailImageClasses = 'Thumbnail rounded-4 border border-2 border-light border-opacity-75 shadow';
+    return (
+        isInnerLink
+        ?
+        <Link
+        to={ to }
+        className={ thumbnailButtonClasses }
+        type='button'>
+            <iframe
+            className={ thumbnailImageClasses }
+            src={ to.pathname }
+            style={{ pointerEvents: 'none' }}
+            scrolling='no'></iframe>
+        </Link>
+        :
+        <a
+        href={ to.pathname }
+        target='_blank'
+        className={ thumbnailButtonClasses }
+        type='button'>
+            <iframe
+            className={ thumbnailImageClasses }
+            src={ to.pathname }
+            style={{ pointerEvents: 'none' }}
+            scrolling='no'></iframe>
+        </a>
+    );
+}
