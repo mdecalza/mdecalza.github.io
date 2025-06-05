765bf9fde8e17b8cf99f5ae23efbddfc87140dbb
Matthew DeCalzadilla <matt.decalzadilla@gmail.com>
2025-06-05 01:29:50 -0600

    Reformatted MusicRelease and BlockQuote components

--- src/components/BlockQuote/BlockQuote.jsx
+++ src/components/BlockQuote/BlockQuote.jsx
@@ -8,7 +8,7 @@ BlockQuote.propTypes = {
 export default function BlockQuote({ text = '', textAlign = 'start' }) {
     return (
         <div
-        className={`${ textAlign === 'center' ? 'text-center' : `${ textAlign === 'end' ? 'text-end' : 'text-start' }` } py-2`}>
+        className={`${ textAlign === 'center' ? 'text-center' : `${ textAlign === 'end' ? 'text-end' : 'text-start' }` }`}>
             <div
             className='blockquote fw-light opacity-75'
             style={{ fontSize: 'clamp(0.85vw, 1.05rem, 4.1vw)' }}>
