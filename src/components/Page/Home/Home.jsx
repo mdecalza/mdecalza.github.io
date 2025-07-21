df72d44e639d321d7fb80281ba49cec64026d25f
Matthew DeCalzadilla <matt.decalzadilla@gmail.com>
2025-07-20 22:00:17 -0600

    Added Second Mix title illustration to Art page

--- src/components/Page/Home/Home.jsx
+++ src/components/Page/Home/Home.jsx
@@ -22,15 +22,18 @@ export default function Home() {
                 <div className='row'>
                     <div className='col-lg py-3'>
                         <div
-                        className='h-100 py-3 rounded-5 shadow'
+                        className='py-3 rounded-5 shadow'
                         style={{ opacity: "100%", backgroundColor: "rgba(255,255,255,0.165)" }}>
                             <Link
                             to={'music'}
                             className='link-light'>
-                                <h2
-                                className='pb-2 growOnHover'>
-                                    Music
-                                </h2>
+                                <div
+                                className="btn border-0 text-white bg-secondary bg-gradient bg-opacity-25 mx-auto mb-3 p-2 rounded-5 shadow-sm growOnHover">
+                                    <h2
+                                    className='mx-3 my-2'>
+                                        Music
+                                    </h2>
+                                </div>
                             </Link>
                             <Socials
                             socialsList={[ 'spotify', 'apple-music', 'music-youtube', 'bandcamp', 'patreon', 'tiktok', 'music-instagram', 'soundcloud', 'twitter' ]} />
@@ -38,15 +41,18 @@ export default function Home() {
                     </div>
                     <div className='col-lg py-3'>
                         <div
-                        className='h-100 py-3 rounded-5 shadow'
+                        className='h-75 py-3 rounded-5 shadow'
                         style={{ backgroundColor: "rgba(255, 255, 255, 0.1175)" }}>
                             <Link
                             to={'art'}
                             className='link-light'>
-                                <h2
-                                className='pb-2 growOnHover'>
-                                    Art
-                                </h2>
+                                <div
+                                className="btn border-0 text-white bg-secondary bg-gradient bg-opacity-25 mx-auto mb-3 p-2 rounded-5 shadow-sm growOnHover">
+                                    <h2
+                                    className='mx-3 my-2'>
+                                        Art
+                                    </h2>
+                                </div>
                             </Link>
                             <Socials
                             socialsList={[ 'art-instagram' ]} />
@@ -54,14 +60,17 @@ export default function Home() {
                     </div>
                     <div className='col-lg py-3'>
                         <div
-                        className='h-100 py-3 rounded-5 bg-secondary bg-opacity-25 shadow'>
+                        className='py-3 rounded-5 bg-secondary bg-opacity-25 shadow'>
                             <Link
                             to={'wads'}
                             className='link-light'>
-                                <h2
-                                className='pb-2 growOnHover'>
-                                    Doom Wads
-                                </h2>
+                                <div
+                                className="btn border-0 text-white bg-secondary bg-gradient bg-opacity-25 mx-auto mb-3 p-2 rounded-5 shadow-sm growOnHover">
+                                    <h2
+                                    className='mx-3 my-2'>
+                                        Doom Wads
+                                    </h2>
+                                </div>
                             </Link>
                             <Socials
                             socialsList={[ 'doomwiki', 'doomworld', 'doom-youtube']} />
@@ -69,14 +78,17 @@ export default function Home() {
                     </div>
                     <div className='col-lg py-3'>
                         <div
-                        className='h-100 py-3 rounded-5 bg-secondary bg-opacity-10 shadow'>
+                        className='py-3 rounded-5 bg-secondary bg-opacity-10 shadow'>
                             <Link
                             to={'websites'}
                             className='link-light'>
-                                <h2
-                                className='pb-2 growOnHover'>
-                                    Websites
-                                </h2>
+                                <div
+                                className="btn border-0 text-white bg-secondary bg-gradient bg-opacity-25 mx-auto mb-3 p-2 rounded-5 shadow-sm growOnHover">
+                                    <h2
+                                    className='mx-3 my-2'>
+                                        Websites
+                                    </h2>
+                                </div>
                             </Link>
                             <Socials
                             socialsList={[ 'linkedin', 'github' ]} />
